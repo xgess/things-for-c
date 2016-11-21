b29cf12ea9d51a24b26509994d5343e45e49391a
Alex Gessner <alex.gessner@gmail.com>
2016-11-21 08:05:36 -0500

    pset3: stupid simple linear search

--- pset3/find/helpers.c
+++ pset3/find/helpers.c
@@ -17,6 +17,11 @@
 bool search(int value, int values[], int n)
 {
     // TODO: implement a searching algorithm
+    for (int i = 0; i < n; i++) {
+        if (values[i] == value) {
+            return true;
+        }
+    }
     return false;
 }
 
