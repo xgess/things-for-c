0df72e8dee50568d0e4e768f748baaae7498612f
Alex Gessner <alex.gessner@gmail.com>
2016-11-21 22:07:02 -0500

    pset3: add insertion sort

--- pset3/find/helpers.c
+++ pset3/find/helpers.c
@@ -8,15 +8,11 @@
  */
        
 #include <cs50.h>
-
+#include <stdio.h>
 #include "helpers.h"
 
-/**
- * Returns true if value is in array of n values, else false.
- */
-bool search(int value, int values[], int n)
-{
-    // TODO: implement a searching algorithm
+
+bool linear_search(int value, int values[], int n) {
     for (int i = 0; i < n; i++) {
         if (values[i] == value) {
             return true;
@@ -25,11 +21,26 @@ bool search(int value, int values[], int n)
     return false;
 }
 
-/**
- * Sorts array of n values.
- */
+void insertion_sort(int values[], int n) {
+    for (int i = 1; i < n; i++) {
+        int insertable_value = values[i];
+
+        for (int j = i; j >= 0; j--) {
+            if (insertable_value < values[j]) {
+                //slide the value at j to the right and plop in insertable_value
+                values[j+1] = values[j];
+                values[j] = insertable_value;
+            }
+        }
+    }
+}
+
+
+bool search(int value, int values[], int n) {
+    return linear_search(value, values, n)
+}
+
 void sort(int values[], int n)
 {
-    // TODO: implement an O(n^2) sorting algorithm
-    return;
+    return insertion_sort(values, n);
 }
