696b6a55f6a7db560378dbf6d32289b9f64aa5b7
Alex Gessner <alex.gessner@gmail.com>
2016-11-21 22:35:57 -0500

    pset3: add binary search

--- pset3/find/helpers.c
+++ pset3/find/helpers.c
@@ -12,6 +12,25 @@
 #include "helpers.h"
 
 
+bool binary_search(int target, int values[], int left_index, int right_index) {
+    int middle_index = (right_index + left_index) / 2;
+
+    if (target == values[middle_index]) {
+        // found it :)
+        return true;
+    } else if (right_index == left_index) {
+        // not gonna find it :(
+        return false;
+    } else if (target < values[middle_index]) {
+        // recurse with the left half
+        return binary_search(target, values, left_index, middle_index - 1);
+    } else if (target > values[middle_index]) {
+        // recurse with the right half
+        return binary_search(target, values, middle_index+1, right_index);
+    }
+    return false;
+}
+
 bool linear_search(int value, int values[], int n) {
     for (int i = 0; i < n; i++) {
         if (values[i] == value) {
@@ -37,10 +56,9 @@ void insertion_sort(int values[], int n) {
 
 
 bool search(int value, int values[], int n) {
-    return linear_search(value, values, n)
+    return binary_search(value, values, 0, n-1);
 }
 
-void sort(int values[], int n)
-{
+void sort(int values[], int n) {
     return insertion_sort(values, n);
 }
